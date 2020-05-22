bcbbf4726d53920c8be89e42475d89388a84a7a0
Fumiaki Kobayashi <fumiaki-kobayashi@mail.nissan.co.jp>
2020-05-22 12:20:25 +0900

    Initial Commit

---
+++ HomeworkDay4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeworkDay4
+//
+//  Created by Fumiaki Kobayashi on 2020/05/22.
+//  Copyright © 2020 Fumiaki Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
