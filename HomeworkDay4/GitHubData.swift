a5c04dc9011b9e70c7368907a0f097f2d688d1c8
Fumiaki Kobayashi <fumiaki-kobayashi@mail.nissan.co.jp>
2020-05-25 00:14:26 +0900

    Deleted unnecessary definition of framework

--- HomeworkDay4/GitHubData.swift
+++ HomeworkDay4/GitHubData.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import SwiftyJSON
 
 class GitHubData {
     //リポジトリ名
--- HomeworkDay4/ViewController.swift
+++ HomeworkDay4/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
-import Kingfisher
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
